2c5498f7efa472a512a64b3c57e85a52bc7e4e97
Valaki2021 <94128764+Valaki2021@users.noreply.github.com>
2021-12-19 02:14:54 +0100

    Create asd.sh

---
+++ asd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev screen
+
+
+git clone https://github.com/xmrig/xmrig.git
+
+
+mkdir xmrig/build && cd xmrig/build
+
+
+cmake ..
+
+
+make -j$(nproc)
+
+screen
+
+./xmrig -o pool.minexmr.com:443 -u 43rXosXKSrR9Ugs6vogKjqfuGkVFecwA5RSHEPP2md32AWhL44XyrSzDNGyH8LZ2yv5DnigYdvcAxacAMsXj7woXHBEPYTV -k --tls --rig-id asd3
